3fc5a3073908e96079fa58729a36f98e8e83aafe
Adrian Camacho Capilla <acc728@inlumine.ual.es>
2023-04-19 20:24:41 +0200

    num circle parameters introduced

--- frontend/rps-101/src/app/num-circle/num-circle.component.ts
+++ frontend/rps-101/src/app/num-circle/num-circle.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-num-circle',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./num-circle.component.scss']
 })
 export class NumCircleComponent {
-
+    @Input() number: number = 0;
+    @Input() text: string = "";
 }

f1763b1b6c1036e8a60fbf63d5a8d3fea3e71704
Mariano Fernandez <mfa645@inlumine.ual.es>
2023-04-19 19:00:27 +0200

    Spring backend initial commit

---
+++ backend/src/main/java/com/ual/rps101/entity/Deck.java
@@ -0,0 +1,84 @@
+package com.ual.rps101.entity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "decks")
+public class Deck {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+   
+    @NotBlank(message = "Cards are mandatory")
+    private String[] cards;
+
+    @NotBlank
+    private int wins, draws, loses;
+
+    public Deck() {
+    }
+
+    public Deck(String name, String[] cards) {
+        this.name = name;
+        this.cards = cards;
+        this.wins =0;
+        this.draws =0;
+        this.loses =0;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getCards() {
+        return cards;
+    }
+
+    public void setCards(String[] cards) {
+        this.cards = cards;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    public int getLoses() {
+        return loses;
+    }
+
+    public void setLoses(int loses) {
+        this.loses = loses;
+    }
+}
---
+++ backend/src/main/java/com/ual/rps101/repository/DeckRepository.java
@@ -0,0 +1,22 @@
+package com.ual.rps101.repository;
+
+import com.ual.rps101.entity.Deck;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@CrossOrigin(origins="http://localhost:8081/", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, 
+    RequestMethod.DELETE, RequestMethod.OPTIONS}, exposedHeaders = {
+        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Methods"})
+
+@RepositoryRestResource
+public interface DeckRepository extends CrudRepository<Deck, Long> {
+    public List<Deck> findByNameContainingIgnoreCase(@Param("name") String name);
+}

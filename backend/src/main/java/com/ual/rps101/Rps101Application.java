7a94127ae63715b96bbd19f4bdc4eeac54a16280
Mariano Fernandez <mfa645@inlumine.ual.es>
2023-04-19 18:05:36 +0200

    New backend project creation

---
+++ backend/src/main/java/com/ual/rps101/Rps101Application.java
@@ -0,0 +1,13 @@
+package com.ual.rps101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rps101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rps101Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ual/rps101/Rps101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ual.rps101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rps101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

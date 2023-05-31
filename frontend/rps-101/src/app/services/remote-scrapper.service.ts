85ddaf799897415c1afd98b41977f023f67aed69
Jose Luis Garrido Castro <jgc450@inlumine.ual.es>
2023-05-31 05:32:22 +0200

    Actualización proyecto arreglado

--- frontend/rps-101/src/app/services/remote-scrapper.service.ts
+++ frontend/rps-101/src/app/services/remote-scrapper.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ScrapperService } from './scrapper.service';
-import { Opponent } from 'app/models/Opponent';
 import { Observable } from 'rxjs';
 import { RemoteApiService } from './remote-api.service';
+import { Opponent } from 'app/models/Opponent';
 
 @Injectable()
 export class RemoteScrapperService implements ScrapperService {
     private scrapperUrl = 'http://localhost:8081/api/bot';
 
     constructor(private remoteApi: RemoteApiService) {}
-
     public getOpponentData(): Observable<Opponent> {
         return this.remoteApi.get<Opponent>(`${this.scrapperUrl}/data`);
     }
+
 }
